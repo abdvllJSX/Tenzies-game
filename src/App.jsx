b3e1424172adc3ca67ee0c9e1a1bda7b21c530ef
cadowlee <alimiabdullahi93@gmail.com>
2023-04-28 22:08:42 +0100

    done for the day

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import "./styles/app.scss"
 import Die from "./components/die"
-import { nanoid } from
- 
-'nanoid
+import { nanoid } from "nanoid"
 
 function App() {
+  function generateNewDie() {
+    const randomNumber = Math.ceil(Math.random() * 6)
+    return {
+      value: randomNumber,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
   const allNewDice = () => {
     const diceArr = []
     for (let i = 0; i < 10; i++) {
-      const randomNumber = Math.ceil(Math.random() * 6)
-      diceArr.push({
-        value: randomNumber, 
-        isHeld: false})
+      diceArr.push(generateNewDie())
     }
     return diceArr
   }
   const [dice, setDice] = useState(allNewDice())
-  const diceElements = dice.map(die => <Die value ={die.value} />)
+  function holdDice(id) {
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ? {
+        ...die,
+        isHeld: !die.isHeld
+      } :
+        die
+    })
+    )
+  }
+  const diceElements = dice.map(die => <Die
+    key={die.id}
+    value={die.value}
+    isHeld={die.isHeld}
+    handleHoldDice={() => holdDice(die.id)}
+  />)
   const rollDice = () => {
-    setDice(allNewDice())
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ? die :
+      generateNewDie()
+    }))
   }
   return (
     <main>
--- src/components/die.jsx
+++ src/components/die.jsx
@@ -3,7 +3,10 @@ import "../styles/die.scss"
 export default function Dice(props){
     return(
         <>
-        <div className="die-face">
+        <div className="die-face" 
+        style={props.isHeld ? {backgroundColor: "#59E391"} : {backgroundColor: "#ffffff"}}
+        onClick={props.handleHoldDice}
+        >
             <h1 className="die-num">{props.value}</h1>
         </div>
         </>
